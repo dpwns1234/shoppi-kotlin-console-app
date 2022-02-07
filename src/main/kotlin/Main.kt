81854afe0ed81d6b1dac0bfdac6fca0ef5b34d04
dpwns1234 <dpwns1234@naver.com>
2022-02-07 19:43:24 +0900

    feat: Learn operator and when

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,4 +9,39 @@ fun main(args: Array<String>) {
         원하시는 카테고리를 선택해주세요
         ***===============================================***
     """.trimIndent())
+    /*
+    카테고리 출력
+    입력 받은 후 저장
+     */
+
+    val categories : Array<String> = arrayOf("패션", "전자기기", "반려동물용품")
+    val category2 = arrayOf("패션", "전자기기", "반려2")
+    val category3 = arrayOf<String>("패현3,", "전자기기3", "반려3")
+    //val intArray = IntArray(3) (1, 2, 0)
+    val falt = FloatArray(3) { 1f; 3f; 2f}
+
+    for(categoryName in categories) {
+        println(categoryName)
+    }
+    println("=> 장바구니로 이동하시려면 #을 입력해주세요")
+    var inputCategory: String? = readLine()
+    while(inputCategory.isNullOrBlank())
+    {
+        println("값을 입력해주세요.")
+        inputCategory = readLine();
+    }
+
+    if(inputCategory == "#") {
+        // TODO 1. 장바구니 이동
+    }
+    else {
+        // TODO 2. 카테고리 상품 목록 보여주기
+        // TODO 3. 주어진 카테고리를 입력하지 않을 경우 예외 처리 해주기
+    }
+
+
+
+
+
+
 }
